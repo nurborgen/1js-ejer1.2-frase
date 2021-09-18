7f207e1759a2aaa8f11c5269538aec044d1a901b
juanseguravasco <jsegura@cipfpbatoi.es>
2021-09-18 15:51:35 +0200

    [MOD] skeleton of functions.js

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,3 +1,39 @@
 'use strict'
 
+function letras(cadena) {
 
+}
+
+function palabras(cadena) {
+
+}
+
+function maysc(cadena) {
+
+}
+
+function titulo(cadena) {
+
+}
+
+function letrasReves(cadena) {
+
+}
+
+function palabrasReves(cadena) {
+
+}
+
+function palindromo(cadena) {
+
+}
+
+module.exports = {
+	letras,
+	palabras,
+	maysc,
+	titulo,
+	letrasReves,
+	palabrasReves,
+	palindromo
+}
